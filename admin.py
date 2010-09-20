4f5a5ba8c3f73e33afc716fc0c7f070d835a1b26
Peter Landry <plandry@provplan.org>
2010-09-20 13:42:03 -0400

    Add Indicator to admin

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from indicators.models import DataSource, IndicatorList
+from indicators.models import DataSource, IndicatorList, Indicator
 
 
 admin.site.register(DataSource)
 admin.site.register(IndicatorList)
-
+admin.site.register(Indicator)
