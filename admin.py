47e464d28622337e18f06cc11250f41f9729a7c7
Peter Landry <plandry@provplan.org>
2010-10-15 14:53:58 -0400

    Added searching on the following fields:
    name'
    datasources__short_name
    short_definition
    long_definition
    notes
    
    resolves #373

--- admin.py
+++ admin.py
@@ -13,6 +13,8 @@ class IndicatorAdmin(admin.ModelAdmin):
     list_display = ('name', 'data_type', 'visible_in_all_lists' )
     list_editable = ('visible_in_all_lists', )
     list_filter = ('data_type', 'visible_in_all_lists', 'datasources')
+    search_fields = ('name', 'datasources__short_name', 'short_definition',
+        'long_definition', 'notes', )
     exclude = ('raw_tags', 'raw_datasources', 'years_available_display', 'years_available', )
     prepopulated_fields = {"slug": ("name",)}
     inlines = [
