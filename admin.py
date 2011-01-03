3b579f5a9b6b39d6d2b8291236a5db778b46c439
Peter L <plandry@provplan.org>
2011-01-03 13:31:49 -0500

    Added the front-end of the new indicator debug batching functionality in the Indicators admin page.
    This will only appear on core instances.

--- admin.py
+++ admin.py
@@ -1,15 +1,45 @@
+import os
+import uuid
+
+from django.conf import settings
 from django.contrib import admin
 from indicators.models import DataSource, IndicatorList, Indicator, IndicatorPregenPart
 
+# Actions available in Core
+def batch_debug_indicators(modeladmin, request, queryset):
+    """ Run selected indicators, and store output and a "debug" csv
+    
+    Each call to this view generates a view batch folder. The contents of the
+    folder:
 
-admin.site.register(DataSource)
-admin.site.register(IndicatorList)
+    - [indicator name].csv for each indicator selected
+    - [indicator name]_debug.csv for each indicator selected
+    - log.txt
+    
+    The indicator generation is scheduled in celery, and output is directed to
+    log.txt (TODO).
+    """
+    from core.indicators import indicator_list
+    
+    # create a directory to store the results of this debug batch
+    batch_id = unicode(uuid.uuid1())
+    batch_folder = os.path.join(settings.MEDIA_ROOT, 'batches', batch_id)
+    os.makedirs(batch_folder)
+
+    # map the queryset to names, for matching against definition classes
+    indicators_to_run = [indicator.name for indicator in queryset]
+
+    [idef(debug=True).csv_output(idef.__name__,path=batch_folder)
+        for idef in indicator_list() 
+        if idef.__name__ in indicators_to_run]
+batch_debug_indicators.short_description = "Run a debug batch on the selected indicators"
 
 def load_indicators(modeladmin, request, queryset):
     from indicators.load import DataImporter
     DataImporter().run_all(indicator_list=queryset)
 load_indicators.short_description = "Load data for the selected Indicators"
 
+# Actions available in Portal
 def publish(modeladmin, request, queryset):
     queryset.update(published=True)
 publish.short_description = "Publish selected indicators"
@@ -36,7 +66,7 @@ class IndicatorAdmin(admin.ModelAdmin):
 
     try:
         from indicators.load import DataImporter
-        actions = [load_indicators, publish, unpublish]
+        actions = [batch_debug_indicators, load_indicators, publish, unpublish]
     except ImportError:
         actions = [publish, unpublish]
     
@@ -79,5 +109,7 @@ class IndicatorAdmin(admin.ModelAdmin):
         )}),
     )
     
-    
+
+admin.site.register(DataSource)
+admin.site.register(IndicatorList)
 admin.site.register(Indicator, IndicatorAdmin)
--- load.py
+++ load.py
@@ -188,117 +188,115 @@ try:
         print "no celery, single indicator: %d" % (end1 - start1).seconds
         print "celery, single indicator: %d" % (end2 - start2).seconds
 except ImportError:
-    pass
+    import re
+    from xml.sax.saxutils import quoteattr
 
-import re
-from xml.sax.saxutils import quoteattr
+    from django.db import IntegrityError  
+    from django.contrib.contenttypes.models import ContentType
 
-from django.db import IntegrityError  
-from django.contrib.contenttypes.models import ContentType
+    from indicators.models import Indicator, IndicatorData
+    from weave.models import AttributeColumn, DataFilter
 
-from indicators.models import Indicator, IndicatorData
-from weave.models import AttributeColumn, DataFilter
+    class WeaveExporter(object):
+        def time_translations(self, input):
+            if not input:
+                return None
+            
+            # school years
+            sy_re = re.compile(r'School Year [0-9]{2}(?P<start_year>[0-9]{2})-[0-9]{2}(?P<end_year>[0-9]{2})')
+            
+            sy_match = sy_re.match(input)
+            if sy_match:
+                return 'SY%s-%s' % (sy_match.group('start_year'), 
+                    sy_match.group('end_year'))
 
-class WeaveExporter(object):
-    def time_translations(self, input):
-        if not input:
-            return None
-        
-        # school years
-        sy_re = re.compile(r'School Year [0-9]{2}(?P<start_year>[0-9]{2})-[0-9]{2}(?P<end_year>[0-9]{2})')
-        
-        sy_match = sy_re.match(input)
-        if sy_match:
-            return 'SY%s-%s' % (sy_match.group('start_year'), 
-                sy_match.group('end_year'))
+            # Calendar Years
+            cy_re = re.compile(r'Calendar Year (?P<year>[0-9]{4})')
 
-        # Calendar Years
-        cy_re = re.compile(r'Calendar Year (?P<year>[0-9]{4})')
+            cy_match = cy_re.match(input)
+            if cy_match:
+                return '%s' % cy_match.group('year')
+            
+            # future conversions...
 
-        cy_match = cy_re.match(input)
-        if cy_match:
-            return '%s' % cy_match.group('year')
-        
-        # future conversions...
+            return input
 
-        return input
-
-    def run(self, indicator_list_qs=Indicator.objects.all()):
-        from django.conf import settings
-        assert hasattr(settings, 'WEAVE')
-        connection = settings.WEAVE.get('CONNECTION', '')
-        indicator_ctype = ContentType.objects.get(app_label='indicators', model='indicator')
-                
-        # create AttributeColumns
-        for indicator in indicator_list_qs.iterator():
-            AttributeColumn.objects.filter(content_type=indicator_ctype,object_id=indicator.id).delete()
-            for indicator_data in indicator.indicatordata_set.values(
-                    'key_unit_type', 'time_key', 'data_type', 'time_type').distinct():
-                try:
-                    if indicator.data_type.upper() == 'NUMERIC':
-                        data_type = 'number'
-                        sql_type = 'numeric'
-                    else:
-                        data_type = 'text'
-                        sql_type = 'string'
-
-                    if indicator_data['time_type'] and indicator_data['time_key']:
-                        time = "%s %s" % (indicator_data['time_type'], indicator_data['time_key'])
-                    else:
-                        time = indicator_data['time_key']
-                    if not indicator_data['time_key'] or indicator_data['time_key'].strip() == '':
-                        time_sql = 'indicators_indicatordata."time_key" is null'
-                    else:
-                        time_sql = 'indicators_indicatordata."time_key" = \'%s\' and indicators_indicatordata."time_type" = \'%s\'' % (indicator_data['time_key'], indicator_data['time_type'])
-                    data_with_keys_query = """
-                        SELECT key_value, %s from indicators_indicatordata WHERE indicators_indicatordata."indicator_id" = '%s' AND indicators_indicatordata."key_unit_type" like '%s' AND (%s)
-                    """ % (
-                        sql_type,
-                        indicator.id,
-                        indicator_data['key_unit_type'],
-                        time_sql,
-                    )
+        def run(self, indicator_list_qs=Indicator.objects.all()):
+            from django.conf import settings
+            assert hasattr(settings, 'WEAVE')
+            connection = settings.WEAVE.get('CONNECTION', '')
+            indicator_ctype = ContentType.objects.get(app_label='indicators', model='indicator')
                     
-                    attribute_column, created = AttributeColumn.objects.get_or_create(
-                        content_type=indicator_ctype,
-                        object_id=indicator.id,
-                        connection=connection,
-                        dataTable=indicator_data['key_unit_type'],
-                        name=indicator.name,
-                        display_name=indicator.weave_name(),
-                        keyType=indicator_data['key_unit_type'],
-                        year=self.time_translations(time) or '',
-                        dataType=data_type,
-                        sqlQuery=data_with_keys_query.strip(),
-                        min=str(indicator.min) if indicator.min else '',
-                        max=str(indicator.max) if indicator.max else ''
-                    )
-                except IntegrityError:
-                    print "Duplicate AttributeColumn detected for (%s, %s, %s). AttributeColumn NOT created." % (
-                        indicator.name,
-                        indicator.key_unit_type,
-                        indicator_data['time_key']
-                    )
-        """    
-        for data_table in DataTable.objects.all():
-            key_unit_type = KeyUnitType.objects.get(name=data_table.key_unit_type)
-            for data_filter in DataFilter.objects.filter(key_unit_type=key_unit_type):
-                filtered_data_table, created = DataTable.objects.get_or_create_for_filter(data_filter)
-                for attribute_column in data_table.attributecolumn_set.all():
-                    attribute_column, created = AttributeColumn.objects.get_or_create(
-                        content_type=attribute_column.content_type,
-                        object_id=attribute_column.object_id,
-                        data_table=filtered_data_table,
-                        name=attribute_column.name,
-                        display_name=attribute_column.display_name,
-                        category=None,
-                        key_unit_type=attribute_column.key_unit_type,
-                        year=attribute_column.year,
-                        data_type=attribute_column.data_type,
-                        data_with_keys_query=data_filter.modify_query(attribute_column.data_with_keys_query),
-                        min=attribute_column.min,
-                        max=attribute_column.max
-                    )
-                         
-        """
+            # create AttributeColumns
+            for indicator in indicator_list_qs.iterator():
+                AttributeColumn.objects.filter(content_type=indicator_ctype,object_id=indicator.id).delete()
+                for indicator_data in indicator.indicatordata_set.values(
+                        'key_unit_type', 'time_key', 'data_type', 'time_type').distinct():
+                    try:
+                        if indicator.data_type.upper() == 'NUMERIC':
+                            data_type = 'number'
+                            sql_type = 'numeric'
+                        else:
+                            data_type = 'text'
+                            sql_type = 'string'
+
+                        if indicator_data['time_type'] and indicator_data['time_key']:
+                            time = "%s %s" % (indicator_data['time_type'], indicator_data['time_key'])
+                        else:
+                            time = indicator_data['time_key']
+                        if not indicator_data['time_key'] or indicator_data['time_key'].strip() == '':
+                            time_sql = 'indicators_indicatordata."time_key" is null'
+                        else:
+                            time_sql = 'indicators_indicatordata."time_key" = \'%s\' and indicators_indicatordata."time_type" = \'%s\'' % (indicator_data['time_key'], indicator_data['time_type'])
+                        data_with_keys_query = """
+                            SELECT key_value, %s from indicators_indicatordata WHERE indicators_indicatordata."indicator_id" = '%s' AND indicators_indicatordata."key_unit_type" like '%s' AND (%s)
+                        """ % (
+                            sql_type,
+                            indicator.id,
+                            indicator_data['key_unit_type'],
+                            time_sql,
+                        )
+                        
+                        attribute_column, created = AttributeColumn.objects.get_or_create(
+                            content_type=indicator_ctype,
+                            object_id=indicator.id,
+                            connection=connection,
+                            dataTable=indicator_data['key_unit_type'],
+                            name=indicator.name,
+                            display_name=indicator.weave_name(),
+                            keyType=indicator_data['key_unit_type'],
+                            year=self.time_translations(time) or '',
+                            dataType=data_type,
+                            sqlQuery=data_with_keys_query.strip(),
+                            min=str(indicator.min) if indicator.min else '',
+                            max=str(indicator.max) if indicator.max else ''
+                        )
+                    except IntegrityError:
+                        print "Duplicate AttributeColumn detected for (%s, %s, %s). AttributeColumn NOT created." % (
+                            indicator.name,
+                            indicator.key_unit_type,
+                            indicator_data['time_key']
+                        )
+            """    
+            for data_table in DataTable.objects.all():
+                key_unit_type = KeyUnitType.objects.get(name=data_table.key_unit_type)
+                for data_filter in DataFilter.objects.filter(key_unit_type=key_unit_type):
+                    filtered_data_table, created = DataTable.objects.get_or_create_for_filter(data_filter)
+                    for attribute_column in data_table.attributecolumn_set.all():
+                        attribute_column, created = AttributeColumn.objects.get_or_create(
+                            content_type=attribute_column.content_type,
+                            object_id=attribute_column.object_id,
+                            data_table=filtered_data_table,
+                            name=attribute_column.name,
+                            display_name=attribute_column.display_name,
+                            category=None,
+                            key_unit_type=attribute_column.key_unit_type,
+                            year=attribute_column.year,
+                            data_type=attribute_column.data_type,
+                            data_with_keys_query=data_filter.modify_query(attribute_column.data_with_keys_query),
+                            min=attribute_column.min,
+                            max=attribute_column.max
+                        )
+                             
+            """
 
