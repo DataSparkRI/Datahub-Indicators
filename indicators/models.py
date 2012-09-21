c023705141cb11c2858fe4fc08476609ee5489e6
Angel Medrano <asmedrano@gmail.com>
2012-09-21 11:08:29 -0400

    Adds some basic tests and updates get_times

--- indicators/models.py
+++ indicators/models.py
@@ -37,17 +37,17 @@ UNIT_CHOICES = (
 )
 
 
-class DataSource(models.Model): 
+class DataSource(models.Model):
     short = models.CharField(max_length=11)
     short_name = models.CharField(max_length=12) # to display in lists, etc
     name = models.CharField(max_length=100)
     url = models.URLField(verify_exists=False)
     icon_file = models.ImageField(upload_to='datasource_icons', blank=True, null=True)
     description = models.TextField(blank=True)
-    
+
     def __unicode__(self):
         return self.name
-    
+
     def icon_path(self):
         return self.icon_file.url
 
@@ -59,7 +59,7 @@ class IndicatorPregenPart(models.Model):
     time_type = models.CharField(max_length=100)
     time_value = models.CharField(max_length=100)
     key_column = models.CharField(max_length=100,blank=True)
-    
+
     def __unicode__(self):
         return u"%s in %s" % (self.column_name, self.file_name)
 
@@ -78,11 +78,11 @@ class IndicatorManager(models.Manager):
         possible_names.append(re.sub(r'Indicator$','',name))
         return self.using(using).get(Q(name__iexact=possible_names[0]) | Q(name__iexact=possible_names[1]))
 
-class Indicator(models.Model):    
+class Indicator(models.Model):
     #qualitative information
     name = models.CharField(max_length=100,blank=False,unique=True) # unique element name, not visible
     file_name = FileNameField(max_length=100, blank=True)
-    display_name = models.CharField(max_length=100)   
+    display_name = models.CharField(max_length=100)
     short_definition = models.TextField()
     long_definition = models.TextField(help_text="This field is Markdown enabled.")
     purpose = models.TextField(blank=True, help_text="This field is Markdown enabled.") # aka rationale/implications
@@ -90,7 +90,7 @@ class Indicator(models.Model):
     limitations = models.TextField(blank=True, help_text="This field is Markdown enabled.")
     routine_use = models.TextField(blank=True)
     last_audited = models.DateTimeField(blank=True, null=True, help_text="Blank or null means it has never been audited")
-    
+
     #quantitative information
     min = models.IntegerField(null=True,blank=True)
     max = models.IntegerField(null=True,blank=True)
@@ -100,9 +100,9 @@ class Indicator(models.Model):
     raw_tags = models.TextField() # will be parsed later into actual tag values
     raw_datasources = models.TextField() # will be parsed into datasource relations
     notes = models.TextField(blank=True) # internal use misc notes
-    
-    
-    
+
+
+
     # calculated meta-data and fields
     data_levels_available = models.CharField(max_length=200, blank=True)
     query_level = models.CharField(max_length=100, blank=True)
@@ -111,22 +111,22 @@ class Indicator(models.Model):
     years_available_display = models.CharField(max_length=200)
     years_available = models.CommaSeparatedIntegerField(max_length=200)
     datasources = models.ManyToManyField(DataSource)
-    
+
     published = models.BooleanField(default=True)
     retired =models.BooleanField(default=False)
     visible_in_all_lists = models.BooleanField(default=False)
-    
+
     slug = models.SlugField(unique=True,db_index=True,null=False)
     tags = TaggableManager(blank=True)
 
     load_pending = models.BooleanField(default=False)
     last_load_completed = models.DateTimeField(null=True,blank=True)
-    
+
     objects = IndicatorManager()
-    
+
     class Meta:
         pass
-    
+
     def weave_name(self):
         return self.display_name
 
@@ -135,10 +135,10 @@ class Indicator(models.Model):
 
     def get_time_types_available(self):
         return self.indicatordata_set.values_list('time_type',flat=True).distinct()
-    
+
     def get_key_unit_types_available(self):
         return self.indicatordata_set.values_list('key_unit_type',flat=True).distinct()
-    
+
     def get_years_available(self):
         return map(lambda sy: school_year_to_year(sy), self.get_time_keys_available())
 
@@ -151,45 +151,41 @@ class Indicator(models.Model):
 
     def get_types_and_times(self):
 
-	def sort_date_range():
-		return
+        def sort_date_range():
+            return
 
-	def compress_date_range(time):
-		return "%s%s" % (time[:5], time[7:]) #2000-2010 -> 2000-10
+        def compress_date_range(time):
+            return "%s%s" % (time[:5], time[7:]) #2000-2010 -> 2000-10
 
-	def time_format(types_and_times_dict):
-		for type_key in types_and_times_dict:
-			if type_key == 'Calendar Year':
-				if not types_and_times_dict[type_key]:
-					del(types_and_times_dict[type_key])
-					continue
-				types_and_times_dict[type_key] = sorted(types_and_times_dict[type_key])
-			elif type_key == 'Multi-Year' or type_key == 'School Year':
-				if not types_and_times_dict[type_key]:
-					del(types_and_times_dict[type_key])
-					continue
-				types_and_times_dict[type_key] = map(lambda x:x[1], sorted(map(lambda a:[map(int,a.split('-')),a], types_and_times_dict[type_key])))
-				types_and_times_dict[type_key] = map(compress_date_range, types_and_times_dict[type_key])
-			elif type_key == 'Term':
-				if not types_and_times_dict[type_key]:
-					del(types_and_times_dict[type_key])
-					continue
-				types_and_times_dict[type_key] = sorted(types_and_times_dict[type_key])
-			else:
-				if not types_and_times_dict[type_key]:
-					del(types_and_times_dict[type_key])
-		return types_and_times_dict
+        def time_format(types_and_times_dict):
 
-	types_and_times_dict = {}
- 	time_types = self.indicatordata_set.values_list('time_type',flat=True).distinct()
-	type_and_times = self.indicatordata_set.filter(time_key__isnull=False).exclude(numeric__isnull=True,string__isnull=True).values_list('time_type', 'time_key').distinct()
-	for time_type in time_types:
-		types_and_times_dict[time_type] = []
-		for type_and_time in type_and_times:
-			a, b = type_and_time
-			if a == time_type:
-				types_and_times_dict[time_type].append(b)
-	return time_format(types_and_times_dict)
+            for type_key in types_and_times_dict:
+
+                if type_key == 'Calendar Year':
+                    types_and_times_dict[type_key] = sorted(types_and_times_dict[type_key])
+
+                elif type_key == 'Multi-Year' or type_key == 'School Year':
+                    types_and_times_dict[type_key] = map(lambda x:x[1], sorted(map(lambda a:[map(int,a.split('-')),a], types_and_times_dict[type_key])))
+                    types_and_times_dict[type_key] = map(compress_date_range, types_and_times_dict[type_key])
+
+                elif type_key == 'Term':
+                    types_and_times_dict[type_key] = sorted(types_and_times_dict[type_key])
+
+            return types_and_times_dict
+
+        types_and_times_dict = {}
+        time_types = self.indicatordata_set.values_list('time_type',flat=True).distinct()
+        type_and_times = self.indicatordata_set.filter(time_key__isnull=False).exclude(numeric__isnull=True, string__isnull=True).values_list('time_type', 'time_key').distinct()
+
+        for time_type in time_types:
+            for type_and_time in type_and_times:
+                a, b = type_and_time
+                if a == time_type:
+                    if time_type not in types_and_times_dict:
+                        types_and_times_dict[time_type] = []
+                    types_and_times_dict[time_type].append(b)
+
+        return time_format(types_and_times_dict)
 
     def get_key_values_available(self, key_unit_type):
         return self.indicatordata_set.values_list('key_value',flat=True).distinct()
@@ -198,10 +194,10 @@ class Indicator(models.Model):
         if not hasattr(self, '_sorted_datasources'):
             self._sorted_datasources = self.datasources.all().order_by('short')
         return self._sorted_datasources
-    
+
     def set_years_available(self):
         """ Collapse a list of years into a single string that spans year ranges
-        
+
         2000,2001,2002,2004 --> 2000-02,2004
         """
         def convert_multi_years_range_to_years(range):
@@ -246,13 +242,13 @@ class Indicator(models.Model):
                         ranged_sets.append("%s-%s" % (start, str(last_year)[2:4]))
                     start = year
                     last_year = year
-        
+
         if last_year == start:
             if start:
                 ranged_sets.append(str(start))
         else:
             ranged_sets.append("%s-%s" % (start, str(last_year)[2:4]))
-        
+
         if len(ranged_sets) > 0:
             self.years_available_display = ','.join(ranged_sets)
         if len(multi_years) > 0:
@@ -279,7 +275,7 @@ class Indicator(models.Model):
         self.set_years_available()
         #self.parse_tags()
         #self.assign_datasources()
-    
+
     def mark_load_complete(self):
         self.load_pending = False
         self.last_load_completed = datetime.datetime.now()
@@ -295,12 +291,12 @@ class Indicator(models.Model):
                 self.datasources.add(DataSource.objects.get(short__iexact=source))
             except DataSource.DoesNotExist:
                 print "Couldn't find datasource to match '%s'" % source
-    
+
     class MultipleDefinitionsFoundException(Exception): pass
 
     def resolve_indicator_def(self):
         """ Resolve the Indicator definition based on name.
-        
+
         Translations to class names for matching:
 
             - Some Indicator classes end with "Indicator"
@@ -319,7 +315,7 @@ class Indicator(models.Model):
 
         possible_names.append(translated_name)
         possible_names.append(translated_name + 'INDICATOR')
-        
+
         resolved_def = None
         for indicator_def in indicator_list(include_new=True):
             if indicator_def.__name__.upper() in possible_names:
@@ -329,7 +325,7 @@ class Indicator(models.Model):
                     print "\t%s\n\n" % indicator_def
                     raise Indicator.MultipleDefinitionsFoundException("Found multiple definitions for this Indicator")
                 resolved_def = indicator_def
-        
+
         return resolved_def
 
     def save(self, *args, **kwargs):
@@ -351,7 +347,7 @@ class IndicatorData(models.Model):
     data_type = models.CharField(max_length=7,choices=DATA_TYPE_CHOICES)
     numeric = RoundingDecimalField(max_digits=20, decimal_places=2, blank=True, null=True)
     string = models.CharField(max_length=100,null=True)
-    
+
     @property
     def value(self):
         # FIXME: why is this still inconsistent? check loading process
@@ -373,7 +369,7 @@ class IndicatorListManager(models.Manager):
         default_list_name = _default_ilist_name(user)
         list, created = self.get_or_create(owner=user, name=default_list_name)
         return list, created
-    
+
     def create_for_user(self, user, name):
         return self.create(
             owner=user,
@@ -390,9 +386,9 @@ class IndicatorList(models.Model):
     created = models.DateField(auto_now_add=True)
     visible_in_weave = models.BooleanField(default=True)
     indicators = models.ManyToManyField(Indicator)
-    
+
     objects = IndicatorListManager()
-    
+
     @property
     def attribute_column_Q(self):
         return Q(content_type=ContentType.objects.get_for_model(Indicator)) & \
@@ -400,7 +396,7 @@ class IndicatorList(models.Model):
                 Q(object_id__in=self.indicators.values_list('id',flat=True)) | \
                 Q(object_id__in=Indicator.objects.filter(visible_in_all_lists=True))
             )
-   
+
     @property
     def can_be_deleted(self):
         return self.name != _default_ilist_name(self.owner)
@@ -412,7 +408,7 @@ class IndicatorList(models.Model):
 
     def __unicode__(self):
         return self.name
-    
+
     @models.permalink
     def get_absolute_url(self):
         return ('indicators-indicator_list', [], {'indicator_list_slug': self.slug})
@@ -426,8 +422,8 @@ class AnonymizedEnrollmentManager(models.Manager):
     def _insert_batch(self, cursor, columns, rows):
         copy_contents = StringIO('%s\n\.\n' % '\n'.join(rows))
         cursor.copy_from(
-            copy_contents, 
-            'indicators_anonymizedenrollment', 
+            copy_contents,
+            'indicators_anonymizedenrollment',
             columns=columns
         )
 
@@ -435,13 +431,13 @@ class AnonymizedEnrollmentManager(models.Manager):
         """ Expects a list of dict-based record definitions """
         from django.db import connections, transaction
         cursor = connections['portal'].cursor()
-    
+
         batch_size = 100000
         row_sqls = []
         count = 0
-        columns = ('id', '"school_year"', '"SASID"', '"distCode"', '"grade"', 
+        columns = ('id', '"school_year"', '"SASID"', '"distCode"', '"grade"',
                    '"enroll_date"', '"exit_date"', '"exit_type"')
-        
+
         sasid_map = {'max': 0}
         def _munge_SASID(sasid):
             """ Use a simple map to munge SASIDs in the anonymized dataset """
@@ -456,7 +452,7 @@ class AnonymizedEnrollmentManager(models.Manager):
             if val is None:
                 return '\N'
             return str(val)
-        
+
         for record in records:
             count += 1
             row_sqls.append("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (
@@ -469,13 +465,13 @@ class AnonymizedEnrollmentManager(models.Manager):
                 _prep(record[5]),
                 _prep(record[6]),
             ))
-            
+
             if count % batch_size == 0:
                 self._insert_batch(cursor, columns, row_sqls)
                 row_sqls = []
                 print "Inserted batch..."
         self._insert_batch(cursor, columns, row_sqls)
-        
+
         connections['portal'].connection.commit()
         print "Complete"
 
@@ -498,7 +494,7 @@ class AnonymizedEnrollmentManager(models.Manager):
 
         start_exit_date =   datetime.date(year, 10, 2)
         end_exit_date =     datetime.date(year + 1, 10, 1)
-        
+
         return self.filter(
             enroll_date__range=(start_enroll_date, end_enroll_date)
         ).filter(
@@ -520,5 +516,5 @@ class AnonymizedEnrollment(models.Model):
     enroll_date = models.DateField(null=True)
     exit_date = models.DateField(null=True)
     exit_type = models.CharField(max_length=200)
-    
+
     objects = AnonymizedEnrollmentManager()
--- indicators/tests.py
+++ indicators/tests.py
@@ -1,9 +1,46 @@
-
 from django.test import TestCase
+from indicators.models import *
+
 
 class IndicatorTest(TestCase):
-    def test_indicator_resolution(self):
+
+    def setUp(self):
+        """ Put stuff here to happen before the tests run
+            Ex: self.test_indicator = Indicator(bleh=1, blah="12")
         """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+        pass
+
+    def test_indicator_get_time_types(self):
+        ind = Indicator(name="Test Indicator")
+        ind.display_name = "test indicator"
+        ind.short_definition = "Short definition"
+        ind.long_definition = "This is the Long Def"
+        ind.unit = "percent"
+        ind.save()
+
+        ind_data = IndicatorData(indicator=ind)
+        ind_data.time_type = 'Multi-Year, '
+        ind_data.time_key = '2009-2010'
+        ind_data.string = "1"
+        ind_data.save()
+
+        ind_data = IndicatorData(indicator=ind)
+        ind_data.time_type = 'Calendar Year, '
+        ind_data.time_key = '2000.0'
+        ind_data.string = "1"
+        ind_data.save()
+
+        ind_data = IndicatorData(indicator=ind)
+        ind_data.time_type = 'School Year, '
+        ind_data.time_key = '2006'
+        ind_data.string = "1"
+        ind_data.save()
+
+        result = ind.get_types_and_times()
+        print result
+        #compare_to = {u'Multi-Year': [u'2009-10']}
+
+        #self.assertEqual(result, compare_to)
+
+
+
