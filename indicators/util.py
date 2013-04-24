4e8144eef20683509ad76bfab29166d26f2f3f6b
Angel Medrano <asmedrano@gmail.com>
2013-04-24 12:00:36 -0400

    Integrated weave filters

--- indicators/util.py
+++ indicators/util.py
@@ -73,16 +73,24 @@ def round_value_for_unit(value, unit):
 def generate_weave(verbose=False):
     """ Write weave tables into weave instance"""
     from weave.api import clear_generated_meta, get_or_create_data_table, insert_data_row
+    from weave.models import DataFilter
     # we need to delete all the hub create weave data
-    print_v(verbose, "Clearing Existing Meta")
+    print_v(verbose, "Clearing Existing Meta...")
     clear_generated_meta()
     table_keys = {}
-
+    filter_keys = {}
     # generatate categories
+    print_v(verbose, "Creating Categories...")
     for kut in IndicatorData.objects.only('key_unit_type').distinct('key_unit_type'):
         table_keys[kut.key_unit_type] = get_or_create_data_table(kut.key_unit_type)
 
+    print_v(verbose, "Collection filters...")
+    for f in DataFilter.objects.all():
+        filter_keys[f.key_unit_type] = {'name':f.name, 'filter_id':f.id, 'table_id':get_or_create_data_table("%s - %s" % (f.key_unit_type, f.name))}
+
+
     # generate data
+    print_v(verbose, "Creating Data...")
     for ind in Indicator.objects.filter(published=True):
         for ind_data in IndicatorData.objects.filter(indicator=ind).distinct('time_key'):
             parent_id = table_keys[ind_data.key_unit_type]
@@ -91,6 +99,18 @@ def generate_weave(verbose=False):
                     format(ind.data_type, ind.id, ind_data.key_unit_type, ind_data.time_key)
 
             insert_data_row(parent_id, title, ind.data_type, sql)
+
+            # apply filters
+            if ind_data.key_unit_type in filter_keys:
+                parent_id = filter_keys[ind_data.key_unit_type]['table_id']
+                title = "%s %s" % (ind.display_name, ind_data.time_key)
+                sql = """SELECT "key_value", "{0}" FROM "public"."indicators_indicatordata" WHERE "indicator_id"='{1}' AND "key_unit_type"='{2}' AND "time_key"='{3}' AND key_value in (SELECT key_value FROM weave_datafilterkey WHERE data_filter_id={4}) """.\
+                    format(ind.data_type, ind.id, ind_data.key_unit_type, ind_data.time_key,filter_keys[ind_data.key_unit_type]['filter_id'])
+
+                insert_data_row(parent_id, title, ind.data_type, sql)
+
+
+
 
 def print_v(verbose, text):
     if verbose:
