5a8c212416ab00706287bac51ff09374d506370e
Angel Medrano <asmedrano@gmail.com>
2013-04-30 16:55:22 -0400

    Updates generate weave

--- indicators/util.py
+++ indicators/util.py
@@ -73,7 +73,7 @@ def round_value_for_unit(value, unit):
 def generate_weave(verbose=False):
     """ Write weave tables into weave instance"""
     from weave.api import clear_generated_meta, get_or_create_data_table, insert_data_row
-    from weave.models import DataFilter
+    from weave.models import DataFilter, WeaveMetaPublic
     # we need to delete all the hub create weave data
     print_v(verbose, "Clearing Existing Meta...")
     clear_generated_meta()
@@ -84,32 +84,44 @@ def generate_weave(verbose=False):
     for kut in IndicatorData.objects.only('key_unit_type').distinct('key_unit_type'):
         table_keys[kut.key_unit_type] = get_or_create_data_table(kut.key_unit_type)
 
-    print_v(verbose, "Collection filters...")
-    for f in DataFilter.objects.all():
-        filter_keys[f.key_unit_type] = {'name':f.name, 'filter_id':f.id, 'table_id':get_or_create_data_table("%s - %s" % (f.key_unit_type, f.name))}
+    print_v(verbose, "Creating filters...")
 
+    for f in DataFilter.objects.all():
+        if f.key_unit_type not in filter_keys:
+            filter_keys[f.key_unit_type] = []
+        filter_keys[f.key_unit_type].append({'kut':f.key_unit_type,'name':f.name, 'filter_id':f.id, 'table_id':get_or_create_data_table("%s - %s" % (f.key_unit_type, f.name))})
 
     # generate data
     print_v(verbose, "Creating Data...")
     for ind in Indicator.objects.filter(published=True):
         for ind_data in IndicatorData.objects.filter(indicator=ind).distinct('time_key'):
+            if ind_data.key_unit_type == 'School':
+                time = format_school_year(ind_data.time_key)
+            else:
+                time = ind_data.time_key
+
             parent_id = table_keys[ind_data.key_unit_type]
-            title = "%s %s" % (ind.display_name, ind_data.time_key)
+            title = "%s, %s" % (ind.display_name, time)
+
             sql = """SELECT "key_value", "{0}" FROM "public"."indicators_indicatordata" WHERE "indicator_id"='{1}' AND "key_unit_type"='{2}' AND "time_key"='{3}'""".\
                     format(ind.data_type, ind.id, ind_data.key_unit_type, ind_data.time_key)
 
-            insert_data_row(parent_id, title, ind.data_type, sql)
+            insert_data_row(parent_id, title, ind.display_name,  ind.data_type, sql, ind.id, time, ind_data.key_unit_type)
 
             # apply filters
             if ind_data.key_unit_type in filter_keys:
-                parent_id = filter_keys[ind_data.key_unit_type]['table_id']
-                title = "%s %s" % (ind.display_name, ind_data.time_key)
-                sql = """SELECT "key_value", "{0}" FROM "public"."indicators_indicatordata" WHERE "indicator_id"='{1}' AND "key_unit_type"='{2}' AND "time_key"='{3}' AND key_value in (SELECT key_value FROM weave_datafilterkey WHERE data_filter_id={4}) """.\
-                    format(ind.data_type, ind.id, ind_data.key_unit_type, ind_data.time_key,filter_keys[ind_data.key_unit_type]['filter_id'])
+                # apply the filters available for this Key Unit Type
+                for kut in filter_keys[ind_data.key_unit_type]:
 
-                insert_data_row(parent_id, title, ind.data_type, sql)
+                    parent_id = kut['table_id']
+                    title = "%s %s" % (ind.display_name, time)
 
+                    sql = """SELECT "key_value", "{0}" FROM "public"."indicators_indicatordata" WHERE "indicator_id"='{1}' AND "key_unit_type"='{2}' AND "time_key"='{3}' AND key_value in (SELECT key_value FROM weave_datafilterkey WHERE data_filter_id={4}) """.\
+                        format(ind.data_type, ind.id, ind_data.key_unit_type, ind_data.time_key, kut['filter_id'])
 
+                    data_table = "%s - %s" % (ind_data.key_unit_type, kut['name'])
+
+                    insert_data_row(parent_id, title, ind.display_name, ind.data_type, sql, ind.id, time, data_table)
 
 
 def print_v(verbose, text):
@@ -117,6 +129,13 @@ def print_v(verbose, text):
         print text
 
 
+def format_school_year(time_key):
+    s = time_key.replace(' ','')
+    s = s.split('-')
+    try:
+        return "SY%s-%s" % (s[0][2:], s[1][2:])
+    except IndexError:
+        return time_key
 
 
 
