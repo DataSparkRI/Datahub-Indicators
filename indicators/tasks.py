db92ed029dcda334524667179e77d06552a8a8cd
Angel Medrano <asmedrano@gmail.com>
2014-04-16 14:23:48 -0400

    Update tasks to decode strings before inserting them for bulk import

--- indicators/tasks.py
+++ indicators/tasks.py
@@ -5,6 +5,9 @@ import time
 from django.db import reset_queries
 from django.conf import settings
 from django.core import management
+from django.db import transaction
+
+
 try:
     from core.models import IndicatorResult
 except ImportError:
@@ -12,7 +15,7 @@ except ImportError:
 from indicators.util import get_dynamic_indicator_def, generate_indicator_data
 from indicators.models import Indicator, IndicatorData
 
-import ucsv as csv
+import csv
 from util import generate_weave
 
 
@@ -149,82 +152,90 @@ try:
 
     @task()
     def import_indicator_csv_task(file_name):
-        count = 0
-        f = open(file_name)
-        reader = csv.DictReader(f)
-        bool_fields = ['published', 'retired', 'visible_in_all_lists', 'load_pending']
-        inds = []
-        for row in reader:
-            count += 1
-            args = dict(row)
-
-            # we need to clean up some of the fields
-            try:
-                del args['last_audited'] # this should be set manually
-            except KeyError:
-                pass
-            try:
-                del args['Column name']
-            except KeyError:
-                pass
-            try:
-                del args['id']
-            except KeyError:
-                pass
-            try:
-                del args['last_load_completed']
-            except KeyError:
-                pass
-
-            # tags in csv is actuall raw_tags so we need to set that up in
-            # args and clear out ['tags']
-            try:
-                args['raw_tags'] = ','.join(args['tags'].split('|'))
-                del args['tags']
-            except KeyError:
-                args['raw_tags'] = ','.join(args['raw_tags'].split('|'))
-
-            if args['min'] == '':
-                args['min'] = None
-            else:
-                args['min'] = int(args['min'])
-
-            if args['max'] == '':
-                args['max'] = None
-            else:
-                args['max'] = int(args['max'])
-
-            if args['suppression_numerator'] == '':
-                args['suppression_numerator'] = None
-            else:
-                args['suppression_numerator'] = int(args['suppression_numerator'])
-
-            if args['suppression_denominator'] == '':
-                args['suppression_denominator'] = None
-            else:
-                args['suppression_denominator'] = int(args['suppression_denominator'])
-
-            # Translate bools fields. If some form of True is not specified, we
-            # default to False
-            for f in bool_fields:
-                if args[f] in ['t', 'true', 'True', 'TRUE', '1']:
-                    args[f] = True
-                else:
-                    args[f] = False
-            try:
-                ind = Indicator(**args)
-                ind.save()
-                inds.append(ind)
-            except TypeError as err:
-                # error in header, end request
-                f.close()
-                return "Could not import from csv. Please check csv header. Error:%s" % err
-
-        for i in inds:
-            i.parse_tags()
-
-        #f.close()
-        return str(count) + "---" + file_name
+        import_indicator_csv(file_name)
 
 except ImportError:
     pass
+
+
+def import_indicator_csv(file_name):
+    count = 0
+    f = open(file_name)
+    reader = csv.DictReader(f)
+    bool_fields = ['published', 'retired', 'visible_in_all_lists', 'load_pending']
+    inds = []
+    for row in reader:
+        count += 1
+        args = dict(row)
+
+        # we need to clean up some of the fields
+        try:
+            del args['last_audited'] # this should be set manually
+        except KeyError:
+            pass
+        try:
+            del args['Column name']
+        except KeyError:
+            pass
+        try:
+            del args['id']
+        except KeyError:
+            pass
+        try:
+            del args['last_load_completed']
+        except KeyError:
+            pass
+
+        # tags in csv is actuall raw_tags so we need to set that up in
+        # args and clear out ['tags']
+        try:
+            args['raw_tags'] = ','.join(args['tags'].split('|'))
+            del args['tags']
+        except KeyError:
+            args['raw_tags'] = ','.join(args['raw_tags'].split('|'))
+
+        if args['min'] == '':
+            args['min'] = None
+        else:
+            args['min'] = int(args['min'])
+
+        if args['max'] == '':
+            args['max'] = None
+        else:
+            args['max'] = int(args['max'])
+
+        if args['suppression_numerator'] == '':
+            args['suppression_numerator'] = None
+        else:
+            args['suppression_numerator'] = int(args['suppression_numerator'])
+
+        if args['suppression_denominator'] == '':
+            args['suppression_denominator'] = None
+        else:
+            args['suppression_denominator'] = int(args['suppression_denominator'])
+
+
+        # ENCODE or Ignore description fields
+        args["short_definition"] = args["short_definition"].decode("utf-8", "ingore")
+        args["long_definition"] = args["long_definition"].decode("utf-8", "ignore")
+
+        # Translate bools fields. If some form of True is not specified, we
+        # default to False
+        for bf in bool_fields:
+            if args[bf] in ['t', 'true', 'True', 'TRUE', '1']:
+                args[bf] = True
+            else:
+                args[bf] = False
+        try:
+            with transaction.commit_on_success():
+                ind = Indicator(**args)
+                ind.save()
+                inds.append(ind)
+        except Exception as err:
+            print(err)
+
+    for i in inds:
+        i.parse_tags()
+
+    f.close()
+    return str(count) + "---" + file_name
